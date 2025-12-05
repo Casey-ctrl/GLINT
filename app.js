ad977969892cd03b9417f670ea87d0974b80fd2c
Casey <collinsdidi@gmail.com>
2025-12-05 08:12:02 +0000

    Nav bar functionalities

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li a');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+hamburger.addEventListener('click', ()=>{
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('open');
 });
 
-links.forEach(link => {
+
+document.querySelectorAll('.nav-links li a').forEach(link => {
   link.addEventListener('click', () => {
-    navLinks.classList.remove('show');
+    hamburger.classList.remove('active');
+    navLinks.classList.remove('open');
   });
 });
+// const navLinks = document.querySelector('.nav-links');
+// const links = document.querySelectorAll('.nav-links li a');
+// const hamburgerClose = document.getElementById('close-btn');
+
+// hamburger.addEventListener('click', () => {
+//   navLinks.classList.toggle('show');
+// });
+// hamburgerClose.addEventListener('click', ()=>{
+//   navLinks.classList.remove('show');
+// })
+
+// links.forEach(link => {
+//   link.addEventListener('click', () =>{
+//     navLinks.classList.remove('show');
+//   });
+// });
